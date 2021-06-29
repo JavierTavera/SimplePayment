84d52b1de1c4d2f1b6090d625c455c3c2a7c3a73
Javier T <javieriti@gmail.com>
2021-06-29 19:05:42 -0400

    5th Commit: Changed shape of floating button

--- app/src/main/java/com/example/simplepayment/MainActivity.java
+++ app/src/main/java/com/example/simplepayment/MainActivity.java
@@ -2,13 +2,11 @@ package com.example.simplepayment;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
@@ -49,8 +47,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
 
         return false;
-
-
     }
+
+
 
 }
